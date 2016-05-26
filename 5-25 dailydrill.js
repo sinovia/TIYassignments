4f8a48390c3dca9379b402fa162ca165c45c98b1
SINOVIA <somayfield@gmail.com>
2016-05-26 10:36:02 -0400

    Snacks for Life

---
+++ 5-25 dailydrill.js
@@ -0,0 +1,7 @@
+var currentYear = 2016
+var birthYear = 1988
+
+var age1 = (currentYear - birthYear);
+var age2=  (currentYear - birthYear +-1);
+
+console.log ("They are either " + age2 + " or " + age1 +" years old")
---
+++ 5-26 dailydrill.js
@@ -0,0 +1,13 @@
+var snackmath =  {fromToday: 28, untilTheDayIDie: 96}
+
+var yearsleft = snackmath.untilTheDayIDie - snackmath.fromToday;
+
+var snackprice = 4.5
+
+var annualsnack= (snackprice * 365)
+
+console.log(yearsleft)
+
+var priceOfLifeTimeSnack = (yearsleft * annualsnack)
+
+console.log(priceOfLifeTimeSnack)

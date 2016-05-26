d785d423455055273e732ec376469df1b08f32f1
SINOVIA <somayfield@gmail.com>
2016-05-26 10:44:23 -0400

    After standups

--- 5-26 dailydrill.js
+++ 5-26 dailydrill.js
@@ -6,8 +6,19 @@ var snackprice = 4.5
 
 var annualsnack= (snackprice * 365)
 
-console.log(yearsleft)
+var priceOfLifeTimeSnack = (yearsleft * annualsnack)
+
+// After standup
+
+var snackmath =  {fromToday: 28, untilTheDayIDie: 96}
+
+var yearsleft = snackmath.untilTheDayIDie - snackmath.fromToday;
+
+var snackprice = 4.5
+
+var annualsnack= (snackprice * 365)
+
 
 var priceOfLifeTimeSnack = (yearsleft * annualsnack)
 
-console.log(priceOfLifeTimeSnack)
+console.log("You will need " + priceOfLifeTimeSnack + " dollars" + " to snack until you die" + " at age " + snackmath.untilTheDayIDie)

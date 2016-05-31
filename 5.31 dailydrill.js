1d7f1809f752d86a07f69859a960843439392dbb
SINOVIA <somayfield@gmail.com>
2016-05-31 10:56:03 -0400

    Language Info

---
+++ 5.31 dailydrill.js
@@ -0,0 +1,11 @@
+
+var greeting = [
+   {language:"en", greeting: "Hola, Mundo!"},
+   {language:"de", greeting: "Hallo Welt!"},
+   {language:"en", greeting: "Hello World!"},
+   ];
+
+for (var i = 0; i < greeting.length; i++) {
+ var greetingLanguage = greeting[i];
+}
+console.log(greeting)

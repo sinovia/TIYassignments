e004a220cc55ce087a2541daa793551a7855fb54
SINOVIA <somayfield@gmail.com>
2016-06-01 10:26:39 -0400

    June 1st daily drill

---
+++ 6-1 dailydrill.js
@@ -0,0 +1,11 @@
+var number = prompt ("Enter number");
+console.log (number);
+var noun = prompt ("Enter noun");
+console.log (noun);
+
+if (number > 1) {
+	console.log (number + " " + noun + "s");
+
+} else {
+	console.log (number +" " + noun);
+}

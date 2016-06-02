a2a8b6cf70489cb27b950d3b33b7b5424f01fbcf
SINOVIA <somayfield@gmail.com>
2016-06-02 11:24:49 -0400

    Added June 2nd drill

---
+++ 6-2 Daily Drill.js
@@ -0,0 +1,14 @@
+var num= []
+
+function () {
+for  (i=0; i < 21; i++)
+ console.log (i);
+	num.push (i);
+}
+
+// (i).each(function(i, el){
+// 	if (i %2 === 0) {
+// 		console.log (Even);
+// 	}
+
+// });

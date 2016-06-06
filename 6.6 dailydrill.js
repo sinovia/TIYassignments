3628f064633d330b9f67f7bc13de9d31864da587
SINOVIA <somayfield@gmail.com>
2016-06-06 10:34:37 -0400

    6-6 daily drill

---
+++ 6.6 dailydrill.js
@@ -0,0 +1,8 @@
+var colors = ["purple", "blue", "red", "green"];
+var arrayLength = colors.length;
+console.log("My favorite color is " + colors [0]);
+
+ {console.log ("If there is no " + colors[0] + "," + " my second choice is " + colors[1]);
+	console.log ("If I can't find " + colors[1] + "," + " my third choice is " + colors[2]);
+	console.log ("If I cannot find " + colors[2] + "," + " I will settle for " + colors[3]);
+}

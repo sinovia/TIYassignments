4a848deb28af252cfae7e70e896056303675eabf
SINOVIA <somayfield@gmail.com>
2016-06-07 10:45:55 -0400

    6-7 daily drill

---
+++ 6-7 dailydrill.js
@@ -0,0 +1,10 @@
+	var recipe = 
+	{title: "Mole",
+	serves: 2,
+	ingredients: ["cinnamon", "cumin", "cocoa"],}
+
+	console.log (recipe.title);
+	console.log ("Serves: " + recipe.serves);
+	console.log (recipe.ingredients[0]);
+	console.log (recipe.ingredients[1]);
+	console.log (recipe.ingredients[2]);

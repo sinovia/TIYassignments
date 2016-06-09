7408adbfeec028446f5f9f35b37e949381bae454
SINOVIA <somayfield@gmail.com>
2016-06-09 10:43:56 -0400

    6-9 daily drill

---
+++ 6-9 daily drills.js
@@ -0,0 +1,10 @@
+
+var drEvilAmount = prompt ("How many dollars?");
+
+	if (drEvilAmount != 1000000) {
+		console.log (drEvilAmount + " dollars");
+	}
+
+else {
+		console.log (drEvilAmount + " dollars" + " pinky");
+}

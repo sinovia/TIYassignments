e96739377d5bcbbe20f9845c06e92f1b81ad3360
SINOVIA <somayfield@gmail.com>
2016-06-14 10:42:56 -0400

    6-14 daily drill

---
+++ 6-14dailydrill.js
@@ -0,0 +1,15 @@
+// var str1 = "Hello ";
+// var str2 = "world!";
+// var res = str1.concat(str2);
+
+
+var word1 = "mix";
+var word2= "pod"
+var word3= (word2[0]+word2[1]+word1[2])
+var word4= (word1[0]+word1[1])+word2[2]
+var concat = word4.concat(word3)
+
+console.log (word1.concat(word2));
+
+console.log(word3)
+console.log(word4)
